ac133fdf1c0cb8b3934ae4cf900bfc61dee0134e
Sukhanov Dmitry <79788948+Rapira16@users.noreply.github.com>
2024-11-03 18:10:00 +0300

    Add files via upload

---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+
+	public class GuestResponse
+	{
+
+		[Required(ErrorMessage = "Введите имя")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Введите почту")]
+		[RegularExpression(".+\\@.+\\..+",
+			ErrorMessage = "Некоректная почта. Пример: mailname@mail.ru")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Введите номер телефона")]
+		[RegularExpression("^\\+?\\d{1,3}[-.\\s]?\\(?\\d{1,4}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$",
+			ErrorMessage ="Некоректный номер телефона. Пример: +7 800 555 35 35")]
+		public string Phone { get; set; }
+
+		[Required(ErrorMessage = "Вы будете присутствовать или нет?")]
+		public bool? WillAttend { get; set; }
+	}
+}
